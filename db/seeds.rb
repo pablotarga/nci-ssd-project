d614dae8ced81bfde5f3348d9b66321bdbd663d1
Pablo Targa <pablo.targa@gmail.com>
2020-04-15 21:19:25 +0100

    change seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-
-if Rails.env.test?
+unless Product.exists?
   Product.create([
-    {title: "God of War 3", quantity: 100, price: 49.99, cost: 23.00, description: 'Short description...'},
-    {title: "Borderland 3", quantity: 73, price: 49.99, cost: 29.00, description: 'Shoot-and-loot'},
-    {title: "Mario Kart 8", quantity: 23, price: 49.99, cost: 24.00, description: 'It\'s a me!'},
-    {title: "PS4 Pro", quantity: 30, price: 499.99, cost: 310.00, description: 'Game console'}
+    {title: "God of War 3", tags: %w[action single\ player ps4] quantity: 100, price: 49.99, cost: 23.00, description: 'Short description...'},
+    {title: "Borderland 3", tags: %w[shoot-and-loot ps4] quantity: 73, price: 49.99, cost: 29.00, description: 'Shoot-and-loot'},
+    {title: "Mario Kart 8", tags: %w[multi-player nintendo\ switch racing] quantity: 23, price: 49.99, cost: 24.00, description: 'It\'s a me!'},
+    {title: "PS4 Pro", tags: %w[console] quantity: 30, price: 499.99, cost: 310.00, description: 'Game console'}
   ])
 end
